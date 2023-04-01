29de9f614c06b7a104451ed1ffbed289117100c0
Антон Ермоленко <darkraven653@gmail.com>
2023-04-01 14:12:32 +0800

    Add models

---
+++ TGBot/Tag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGBot
+{
+    class Tag:Modal<int>
+    {
+        private string name;
+        private double relevance;
+
+        Tag(int id, string name, double relevance)
+            :base(id)
+        {
+            this.id = id;
+            this.name = name;
+            this.relevance = relevance;
+        }
+
+        protected override string ToString()
+        {
+            return name + "(" + relevance.ToString() + ")";
+        }
+    }
+}
